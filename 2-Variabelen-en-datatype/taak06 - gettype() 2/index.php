ea4ce7008fd025fef268120ef570ed85b6189dc7
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-01 19:53:28 +0100

    afgemaakt

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$var1 = "I Love PHP";
+$var2 = 100;
+$var3 = 10.5;
+$var4 = false;
+$var5 =  null;
+
+echo gettype($var1) . "<br/>";
+echo gettype($var2) . "<br/>";
+echo gettype($var3) . "<br/>";
+echo gettype($var4) . "<br/>";
+echo gettype($var5) . "<br/>";
+
+?>
