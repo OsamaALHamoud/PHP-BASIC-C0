39f497155e56fded839873471bb3de661c3c5346
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-02 23:52:44 +0100

    Afgemaakt

---
+++ 2-Variabelen-en-datatype/taak11 - var_dump()/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$naam              = "Osama AL Hamoud"; //Data type [1] String
+$nummer            = 5;                 //Data type [2] Integer
+$decimale_getallen = 5.55;              //Data type [3] Floating Point Number
+$var4              = true;              //Data type [4] Bloolean / مشان الترو ول فالاص 
+$var_5             = array(
+    "A" => "V",
+    "B" => 50,
+    "C" => 50.65,
+);
+
+echo "<h2> Get Type </h2>";
+echo gettype($naam)    . "<br/>";
+echo gettype($nummer)  . "<br/>";
+echo gettype($decimale_getallen) . "<br/>";
+echo gettype($var4)    . "<br/>";
+echo gettype($var_5)    . "<br/>";
+
+
+
+
+
+echo "<h2> Varaible var_dump </h2>";
+
+var_dump($naam);  echo "<br/>";
+var_dump($nummer); echo  "<br/>";
+var_dump($decimale_getallen); echo  "<br/>";
+var_dump($var4); echo  "<br/>";
+var_dump($var_5); echo  "<br/>";
