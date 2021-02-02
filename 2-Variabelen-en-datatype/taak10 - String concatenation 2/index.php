fcdadbf64b89e5e00e7134156d46c770d81d77a2
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-02 22:34:51 +0100

    afgemaakt

---
+++ 2-Variabelen-en-datatype/taak10 - String concatenation 2/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// dat is how make Concatention met elkaar
+
+$item = "<ul>";
+$item .= "<li>tas</li>";
+$item .= "<li>pen</li>";
+$item .= "<li>jas</li>";
+$item .= "</ul>";
+
+
+
+echo $item;
+?>
