32933a44b3258eb2fc779dc7bedc849a8218de34
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-03 11:42:49 +0100

    afgemaakt Arithmetic

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$getal_1 = 10;
+$getal_2 = 15;
+
+echo $getal_1 + $getal_2 . "<br/>";
+echo $getal_1 % $getal_2 . "<br/>";
+echo $getal_1 * $getal_2 . "<br/>";
+echo $getal_1 - $getal_2 . "<br/>";
