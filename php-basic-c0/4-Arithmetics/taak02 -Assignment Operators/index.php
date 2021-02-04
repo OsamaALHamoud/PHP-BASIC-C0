65205331465c9ea7a3ddb165ecfa2988fdc76027
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-05 00:20:17 +0100

    afgemaakt

---
+++ php-basic-c0/4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,19 @@
+<?php
+$scor1   = 100;
+$scor2   = 100;
+$scor3   = 100;
+$scor4   = 100;
+
+$scor1   =$scor1 + 10;
+$scor2   =$scor1 / 10;
+$scor3   =$scor1 - 10;
+$scor4   =$scor1 * 10;
+
+
+echo $scor1 . "<br/>"; 
+echo $scor2  . "<br/>";
+echo $scor3  . "<br/>";
+echo $scor4 . "<br/>";
+
+
+?>
