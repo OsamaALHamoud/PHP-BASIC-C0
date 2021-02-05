896fec6cc18edf69d3f8e0faa9ad25e3612fec7d
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-05 10:54:30 +0100

    afgemaakt if else

---
+++ php-basic-c0/5-Conditional_Statements/taak03 - if, else conditional/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$variabele_1 = 10;
+$variabele_2 = 10;
+
+
+if($variabele_1 == $variabele_2){
+    echo "gelijk" . "<br/>";
+}
+else{
+
+echo "ongelijk" ; 
+}
+
+if($variabele_1 > $variabele_2){
+    echo "gelijk" . "<br/>";
+}
+else{
+
+    echo "ongelijk" . "<br/>"; 
+}
+if($variabele_1 <> $variabele_2){
+    echo "gelijk" . "<br/>";
+}
+else{
+
+    echo "is ongelijk aan" . "<br/>"; 
+}
+if($variabele_1 >= $variabele_2){
+    echo "Groeter dan" . "<br/>";
+}
+else{
+
+    echo "ongelijk" . "<br/>"; 
+}
