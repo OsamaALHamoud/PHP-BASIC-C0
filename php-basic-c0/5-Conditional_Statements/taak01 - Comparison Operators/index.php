73e56a79f688bc371cc8845b60a4ce9590b9196d
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-05 04:33:51 +0100

    afgemaakt

--- php-basic-c0/5-Conditional_Statements/taak01 - Comparison Operators/index.php
+++ php-basic-c0/5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -6,9 +6,9 @@ $test2 = "gewoon test";
 
 var_dump($test1>$test2); echo "<br/>";
 
-var_dump($test1>$test2);  echo  "<br>";
-var_dump($test1<>$test2);  echo  "<br>";
-var_dump($test1<>$test2);  echo  "<br>";
-var_dump($test1>$test2);  echo  "<br>";
-var_dump($test1<>$test2);  echo  "<br>";
-var_dump($test1>$test2);  echo  "<br>";
+var_dump($test1>$test2 );    echo  "<br>";
+var_dump($test1<>$test2);    echo  "<br>";
+var_dump($test1<>$test2);    echo  "<br>";
+var_dump($test1>$test2 );    echo  "<br>";
+var_dump($test1<>$test2);    echo  "<br>";
+var_dump($test1>$test2 );    echo  "<br>";
