d72ee952717562745a7131147c8bab3df2c7f177
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-05 05:04:47 +0100

    if Afgemaakt

---
+++ php-basic-c0/5-Conditional_Statements/taak02 - if conditional/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$variabele1 = 6;
+$variabele2 = 15;
+
+
+if ($variabele1 == $variabele2);{
+    echo "gelijk ". "<br/>";
+}
+
+if ($variabele1 == $variabele2);{
+    echo "ongelijk " . "<br/>";
+}
+if ($variabele1 === $variabele2);{
+    echo "dentiek ";
+}
+if ($variabele1 > $variabele2);{
+    echo "test1 is groter dan test2 ". "<br/>";
+}
+if ($variabele1 < $variabele2);{
+    echo "test1 is kleiner dan test2" . "<br/>";
+}
+if ($variabele1 >= $variabele2);{
+    echo "test1 is groter of gelijk aan test2". "<br/>" ;
+}
+if ($variabele1 <= $variabele2);{
+    echo "test1 is kleiner of gelijk aan test2" . "<br/>";
+}
