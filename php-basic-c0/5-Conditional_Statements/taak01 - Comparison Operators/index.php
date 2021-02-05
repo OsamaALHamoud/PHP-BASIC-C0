dc7f812b601d227dc2d8c6543f0916560cbe7f48
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-05 04:32:12 +0100

    afgemaakt

---
+++ php-basic-c0/5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$test1 = 12;
+$test2 = "gewoon test";
+
+
+var_dump($test1>$test2); echo "<br/>";
+
+var_dump($test1>$test2);  echo  "<br>";
+var_dump($test1<>$test2);  echo  "<br>";
+var_dump($test1<>$test2);  echo  "<br>";
+var_dump($test1>$test2);  echo  "<br>";
+var_dump($test1<>$test2);  echo  "<br>";
+var_dump($test1>$test2);  echo  "<br>";
