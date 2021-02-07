2b73d7a0fc5cab822dbee6a90fffe204fed6d5fb
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-07 19:24:41 +0100

    afgemaakt switch

---
+++ php-basic-c0/5-Conditional_Statements/taak05 - switch conditional/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+$famille_namen = "badr"; 
+        
+
+switch($famille_namen) {
+    case "kosai":
+        echo "Kosai studered niet hij wilt altijd spellen";
+        break;
+        case "Shadi":
+        echo " geinet van zijn werk hij wil altijd werken";
+        break;
+        case "badr":
+        echo "Badr hij  vind leuk voor de hele dag films kijken ";
+        break;
+        default:
+        echo "Er is geen namen van u famille ";
+}
