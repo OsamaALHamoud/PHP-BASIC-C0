d828b59216cc1bab1a0df588cc624d17583d96db
OsamaALHamoud <osama.alhamoud@student.rocva.nl>
2021-02-13 05:44:59 +0100

    afgemaakt

---
+++ php-basic-c0/6-Functions/taak02 - strlen()/index.php
@@ -0,0 +1,10 @@
+<?php
+
+function mijnNaamIsGroot($voornaam ,  $achternaam ,) {
+
+    echo " Mijn Naam is " . $voornaam . "" . $achternaam;
+}
+
+mijnNaamIsGroot("Osama ", " Al Hamoud" , "");
+
+ 
